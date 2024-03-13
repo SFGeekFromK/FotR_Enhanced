e8a1a160a3ea9ff5b1ddf2ef3cee807685398c7a
SFGeekFromK <swjacklee@gmail.com>
2024-03-13 18:00:00 +0900

    update interven mission reward arg
    Favourrewards set to default, republic intervention mission reward supportarg(testing) increased to 100

---
+++ Data/Scripts/Library/eawx-plugins/intervention-missions/factions/FactionTables_EMPIRE.lua
@@ -0,0 +1,35 @@
+return {
+	Missions = {
+		["ACCUMULATE"] = {active = false, chance = 4},
+		["CARGO_CONVOY"] = {active = false, chance = 12},
+		["CONQUER"] = {active = false, chance = 12},
+		["CONSTRUCT"] = {active = false, chance = 12},
+		["CREW_RECRUITMENT"] = {active = false, chance = 4},
+		["HERO_CONVOY"] = {active = false, chance = 12},
+		["INFRASTRUCTURE_EXPANSION"] = {active = false, chance = 8},
+		["LIBERATE"] = {active = false, chance = 12},
+--		["RAISE_INFLUENCE"] = {active = false, chance = 8},
+		["RECON"] = {active = false, chance = 12},
+		["UPGRADE"] = {active = false, chance = 12},
+	},
+	RewardGroups = {
+		"REP",
+		"REP",
+		"REP",
+		"REP",
+		"PDF"
+	},
+	RewardGroupDetails = {
+		["REP"] = {
+			DialogName = "REP",
+			RewardName = "ERA",
+			GroupSupport = "SECTOR_FORCES",
+			SupportArg = 100 --1
+		},
+		["PDF"] = {
+			DialogName = "PDF",
+			RewardName = "PDF",
+			GroupSupport = nil,
+		}
+	}
+}

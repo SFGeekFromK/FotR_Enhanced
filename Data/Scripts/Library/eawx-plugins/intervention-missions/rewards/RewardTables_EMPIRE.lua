f211793d4751183c84ac24a885f225491615ee11
SFGeekFromK <swjacklee@gmail.com>
2024-04-20 01:37:55 +0900

    reward table

---
+++ Data/Scripts/Library/eawx-plugins/intervention-missions/rewards/RewardTables_EMPIRE.lua
@@ -0,0 +1,145 @@
+return {
+	Era_1 = {
+		{
+			{"CEC_Light_Cruiser", 4},
+			{"Class_C_Frigate", 5},
+			{"Class_C_Support", 5},			
+			{"Republic_Trooper_Team", 5},
+			{"Republic_Navy_Trooper_Squad", 5},	
+		},
+		{
+			{"Dreadnaught_Lasers", 2},
+			{"Dreadnaught_Carrier", 2},
+			{"Republic_Jedi_Squad", 2},
+			{"Republic_Gian_Company", 5},
+		},
+		{
+			{"Antarian_Ranger_Squad", 5},	
+			{"Special_Tactics_Trooper_Squad", 5},		
+			{"Senate_Commando_Platoon", 5},
+			{"Republic_VAAT_Group", 2},
+			{"Republic_A4_Juggernaut_Company", 2},
+			{"Starbolt", 3},
+		}
+	},
+	Era_2 = {
+		{
+			{"CEC_Light_Cruiser", 4},
+			{"Clone_Special_Ops_Platoon", 5},
+			{"Clone_Galactic_Marine_Platoon", 5},
+			{"Special_Tactics_Trooper_Squad", 5},
+			{"Republic_Navy_Trooper_Squad", 5},
+			{"Republic_Trooper_Team", 5},
+		},
+		{
+			{"Antarian_Ranger_Squad", 5},	
+			{"Senate_Commando_Platoon", 5},
+			{"Clone_Jumptrooper_Phase_One_Company", 3},
+			{"Dreadnaught_Carrier", 2},
+			{"Republic_A5RX_Company", 2},
+			{"Republic_VAAT_Group", 2},
+			{"SPHA_T_Company", 1},
+		},
+		{
+			{"Republic_RX200_Falchion_Company", 1},
+			{"Republic_A6_Juggernaut_Company", 1},
+			{"Starbolt", 3},
+		}
+	},
+	Era_3 = {
+		{
+			{"Customs_Corvette", 4},
+			{"CEC_Light_Cruiser", 4},
+			{"Clone_Special_Ops_Platoon", 5},
+			{"Clone_Galactic_Marine_Platoon", 5},
+			{"Special_Tactics_Trooper_Squad", 5},
+			{"Republic_Navy_Trooper_Squad", 5},
+			{"Republic_Trooper_Team", 5},
+		},
+		{
+			{"Antarian_Ranger_Squad", 5},	
+			{"Senate_Commando_Platoon", 5},
+			{"Clone_Jumptrooper_Phase_One_Company", 3},
+			{"Dreadnaught_Carrier", 2},
+			{"Republic_A5RX_Company", 2},
+			{"Republic_VAAT_Group", 2},
+		},
+		{
+			{"Republic_RX200_Falchion_Company", 1},
+			{"Republic_A6_Juggernaut_Company", 1},
+			{"Starbolt", 3},
+		}
+	},		
+	Era_4 = {
+		{
+			{"Customs_Corvette", 4},
+			{"CEC_Light_Cruiser", 4},
+			{"Clone_Blaze_Trooper_Squad", 5},
+			{"Clone_Galactic_Marine_Platoon", 5},
+			{"Special_Tactics_Trooper_Squad", 5},
+			{"Republic_Navy_Trooper_Squad", 5},
+			{"Republic_Trooper_Team", 5},
+		},
+		{
+			{"Antarian_Ranger_Squad", 5},	
+			{"Senate_Commando_Platoon", 5},
+			{"Clone_Jumptrooper_Phase_Two_Company", 3},
+			{"Republic_A5RX_Company", 2},
+			{"Republic_VAAT_Group", 2},
+		},
+		{
+			{"Republic_RX200_Falchion_Company", 1},
+			{"Republic_A6_Juggernaut_Company", 1},
+			{"Generic_Gladiator", 2},
+			{"Starbolt", 3},
+		}
+	},
+	Era_5 = {
+		{
+			{"Customs_Corvette", 4},
+			{"CEC_Light_Cruiser", 4},
+			{"Clone_Blaze_Trooper_Squad", 5},
+			{"Clone_Galactic_Marine_Platoon", 5},
+			{"Special_Tactics_Trooper_Squad", 5},
+			{"Republic_Navy_Trooper_Squad", 5},
+			{"Republic_Trooper_Team", 5},
+		},
+		{
+			{"Antarian_Ranger_Squad", 5},	
+			{"Senate_Commando_Platoon", 5},
+			{"Clone_Jumptrooper_Phase_Two_Company", 3},
+			{"Republic_A5RX_Company", 2},
+			{"Republic_VAAT_Group", 2},
+		},
+		{
+			{"Republic_RX200_Falchion_Company", 1},
+			{"Republic_A6_Juggernaut_Company", 1},
+			{"Generic_Gladiator", 2},
+			{"Starbolt", 3},
+		}
+	},
+	PDF = {
+		{
+			{"LAC", 5},
+			{"Consular_Refit", 5},
+			{"Class_C_Frigate", 5},
+			{"Customs_Corvette", 5},
+			{"Storm_Cloud_Car_Company", 3},
+			{"Light_Mercenary_Team", 5},
+			{"Mercenary_Team", 5},
+		},
+		{
+			{"Mandalorian_Soldier_Company", 4},
+			{"Mandalorian_Commando_Company", 3},
+			{"Republic_Gian_Company", 5},
+			{"Ailon_Nova_Guard_Squad", 5},
+			{"Sun_Guard_Company", 5},
+		},
+		{
+			{"Kuari_Princess_Liner", 1},
+			{"Invincible_Cruiser", 1},
+			{"Generic_Procurator", 1},
+			{"Elite_Mercenary_Team", 3},
+		}
+	}
+}

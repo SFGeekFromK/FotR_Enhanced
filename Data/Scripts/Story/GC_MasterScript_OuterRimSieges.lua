328516f6a203329bac63a13b8f653717e8177465
SFGeekFromK <swjacklee@gmail.com>
2024-05-16 01:41:01 +0900

    outer rim sieges era setup 5

--- Data/Scripts/Story/GC_MasterScript_OuterRimSieges.lua
+++ Data/Scripts/Story/GC_MasterScript_OuterRimSieges.lua
@@ -134,7 +134,7 @@ end
 
 function State_Framework_Activation(message)
     if message == OnEnter then
-		GlobalValue.Set("CURRENT_ERA", 4)
+		GlobalValue.Set("CURRENT_ERA", 5)
 		crossplot:publish("INITIALIZE_AI", "empty")
 		crossplot:publish("VENATOR_HEROES", "empty")
 		crossplot:publish("VICTORY_HEROES", "empty")

766fb171b73271949ad4c449b2482310dc0965a1
SFGeekFromK <swjacklee@gmail.com>
2024-05-21 01:40:58 +0900

    Wolfpack start

---
+++ Data/Scripts/GameObject/ObjectScript_Wolfpack.lua
@@ -0,0 +1,41 @@
+require("PGStateMachine")
+
+
+function Definitions()
+	Define_State("State_Init", State_Init)
+
+end
+
+function State_Init(message)
+
+	if Get_Game_Mode() ~= "Land" then
+		ScriptExit()
+	end
+
+		
+	if message == OnEnter then
+        Hide_Sub_Object(Object, 1, "jumptroop")
+        Hide_Sub_Object(Object, 1, "body_LOD0")
+		Hide_Sub_Object(Object, 1, "body_LOD1")
+		Hide_Sub_Object(Object, 1, "helmet_LOD0")
+		Hide_Sub_Object(Object, 1, "helmet_LOD1")
+		Hide_Sub_Object(Object, 0, "body_104_LOD0")
+		Hide_Sub_Object(Object, 0, "body_104_LOD1")
+		Hide_Sub_Object(Object, 0, "helmet_104_LOD0")
+		Hide_Sub_Object(Object, 0, "helmet_104_LOD1")
+		-- for snow version
+		Hide_Sub_Object(Object, 1, "Body_LOD0")
+		Hide_Sub_Object(Object, 1, "Body_LOD1")
+		Hide_Sub_Object(Object, 1, "Head_LOD0")
+		Hide_Sub_Object(Object, 1, "Head_LOD1")
+		Hide_Sub_Object(Object, 0, "Head_104_LOD0")
+		Hide_Sub_Object(Object, 0, "Head_104_LOD1")
+        Hide_Sub_Object(Object, 1, "lenseP1")
+        
+        if GlobalValue.Get("CURRENT_CLONE_PHASE") == 2 then -- 
+            Hide_Sub_Object(Object, 1, "lenseP2")
+        end
+		
+        ScriptExit()
+	end
+end

ab8e0044d2e7f1f24c9cae40f17b3baeb6a49453
SFGeekFromK <swjacklee@gmail.com>
2024-10-10 15:28:41 +0900

    decolorized rep space unit lua entries

--- Data/Scripts/Library/eawx-mod-fotr/gameobjects/GENERIC_SECUTOR.lua
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/GENERIC_SECUTOR.lua
@@ -1,13 +1,13 @@
 return {
 	Ship_Crew_Requirement = 35,
 	Fighters = {
-		["TORRENT_SQUADRON"] = {
+		["TORRENT_SQUADRON_IMP"] = {
 			DEFAULT = {Initial = 3, Reserve = 8, TechLevel = LessOrEqualTo(3)}
 		},
-		["GENERIC_V-WING_SQUADRON"] = {
+		["GENERIC_V-WING_SQUADRON_IMP"] = {
 			DEFAULT = {Initial = 3, Reserve = 8, HeroOverride = {{"EMPEROR_PALPATINE"}, {"ELITE_GUARD_NIMBUS_SQUADRON"}}, TechLevel = GreaterThan(3)}
 		},
-		["TWIN_ION_ENGINE_STARFIGHTER_SQUADRON"] = {
+		["TWIN_ION_ENGINE_STARFIGHTER_SQUADRON_IMP"] = {
 			DEFAULT = {Initial = 1, Reserve = 2}
 		},
 		["2_WARPOD_SQUADRON_DOUBLE"] = {
@@ -15,7 +15,7 @@ return {
 			SECTOR_FORCES = {Initial = 1, Reserve = 2},
 			WARLORDS = {Initial = 1, Reserve = 2}
 		},
-		["GENERIC_BTLB_Y-WING_SQUADRON_DOUBLE"] = {
+		["GENERIC_BTLB_Y-WING_SQUADRON_DOUBLE_IMP"] = {
 			CIS = {Initial = 1, Reserve = 2},
 			EMPIRE = {Initial = 1, Reserve = 2, ResearchType = "~RepublicWarpods"},
 			HOSTILE = {Initial = 1, Reserve = 2}
@@ -23,7 +23,7 @@ return {
 		["KIMOGILA_SQUADRON_DOUBLE"] = {
 			HUTT_CARTELS = {Initial = 1, Reserve = 2}
 		},
-		["NTB_630_SQUADRON"] = {
+		["NTB_630_SQUADRON_IMP"] = {
 			DEFAULT = {Initial = 1, Reserve = 4}
 		}
 	},
--- Data/Scripts/Library/eawx-mod-fotr/gameobjects/GENERIC_VENATOR_IMP.lua
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/GENERIC_VENATOR_IMP.lua
@@ -1,20 +1,23 @@
 return {
-     Ship_Crew_Requirement = 10,
+	Ship_Crew_Requirement = 10,
 	Fighters = {
-		["TORRENT_SQUADRON_IMPERIAL"] = {
+		["TORRENT_SQUADRON"] = {
 			DEFAULT = {Initial = 1, Reserve = 4},
 			EMPIRE = {Initial = 1, Reserve = 4, TechLevel = LessOrEqualTo(3)}
 		},
-		["GENERIC_V-WING_SQUADRON_IMPERIAL"] = {
+		["GENERIC_V-WING_SQUADRON"] = {
 			EMPIRE = {Initial = 1, Reserve = 4, HeroOverride = {{"SATE_PESTAGE","MON_MOTHMA"}, {"ELITE_GUARD_NIMBUS_SQUADRON","ELITE_GUARD_NIMBUS_SQUADRON"}}, TechLevel = GreaterThan(3)}
 		},
-		["GENERIC_ARC_170_SQUADRON_IMPERIAL"] = {
+		["GENERIC_ARC_170_SQUADRON"] = {
 			DEFAULT = {Initial = 1, Reserve = 3}
 		},
-		["GENERIC_BTLB_Y-WING_SQUADRON"] = {
-			DEFAULT = {Initial = 1, Reserve = 4}
+		["2_WARPOD_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 4, ResearchType = "RepublicWarpods"}
 		},
-		["NTB_630_SQUADRON_IMPERIAL"] = {
+		["GENERIC_BTLB_Y-WING_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 4, ResearchType = "~RepublicWarpods"}
+		},
+		["NTB_630_SQUADRON"] = {
 			DEFAULT = {Initial = 1, Reserve = 3}
 		}
 	},

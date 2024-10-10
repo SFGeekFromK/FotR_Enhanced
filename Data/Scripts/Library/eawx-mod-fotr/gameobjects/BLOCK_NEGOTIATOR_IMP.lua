d4a963b0d3f034f461cac514be9aa9ee852a6bf6
SFGeekFromK <swjacklee@gmail.com>
2024-10-11 01:00:31 +0900

    grey fighters 1

--- Data/Scripts/Library/eawx-mod-fotr/gameobjects/BLOCK_NEGOTIATOR_IMP.lua
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/BLOCK_NEGOTIATOR_IMP.lua
@@ -1,13 +1,13 @@
 return {
 	Fighters = {
-		["TORRENT_SQUADRON"] = {
+		["TORRENT_SQUADRON_IMP"] = {
 			DEFAULT = {Initial = 1, Reserve = 4},
 			EMPIRE = {Initial = 1, Reserve = 4, TechLevel = LessOrEqualTo(3)}
 		},
-		["REPUBLIC_Z95_HEADHUNTER_SQUADRON_HALF"] = {
+		["TWIN_ION_ENGINE_STARFIGHTER_SQUADRON_HALF_IMP"] = {
 			EMPIRE = {Initial = 2, Reserve = 0, TechLevel = EqualTo(3)}
 		},
-		["REPUBLIC_Z95_HEADHUNTER_SQUADRON"] = {
+		["TWIN_ION_ENGINE_STARFIGHTER_SQUADRON_HALF"] = {
 			EMPIRE = {Initial = 1, Reserve = 4, TechLevel = EqualTo(4)}
 		},
 		["MISSILE_NIMBUS_SQUADRON"] = {

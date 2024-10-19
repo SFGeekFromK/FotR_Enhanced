552224100787b191fc6e112f64c17904b616cb04
SFGeekFromK <swjacklee@gmail.com>
2024-10-19 16:03:54 +0900

    different mesh name added

--- Data/Scripts/Library/eawx-plugins-gameobject-space/decolor-manager/DecolorManager.lua
+++ Data/Scripts/Library/eawx-plugins-gameobject-space/decolor-manager/DecolorManager.lua
@@ -12,7 +12,12 @@ function DecolorManager:new()
 
 		if team_Color == 1 then
 			Hide_Sub_Object(Object, 1, "Origin")
+			Hide_Sub_Object(Object, 1, "central_mesh") -- victory varinats
+			Hide_Sub_Object(Object, 1, "main_hull_mesh") --victory variants
+			
 			Hide_Sub_Object(Object, 0, "Gray")
+			Hide_Sub_Object(Object, 0, "central_mesh_gray")
+			Hide_Sub_Object(Object, 0, "main_hull_mesh_gray")
         end
 end
 

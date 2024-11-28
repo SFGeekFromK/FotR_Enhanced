55e926b8379bf3744a976c50a6c7607d61906d80
SFGeekFromK <swjacklee@gmail.com>
2024-11-29 00:16:08 +0900

    bug fix 4

--- Data/Scripts/Library/eawx-plugins/republic-heroes/RepublicHeroes.lua
+++ Data/Scripts/Library/eawx-plugins/republic-heroes/RepublicHeroes.lua
@@ -550,7 +550,6 @@ function RepublicHeroes:init_heroes()
 		Handle_Hero_Exit("Ahsoka", council_data)
 		Handle_Hero_Exit("Halcyon", council_data)
 		Handle_Hero_Exit("Gregor", commando_data)
-		set_unit_index("Block", 2, admiral_data)
 
 		RepublicHeroes:Add_Fighter_Set("Odd_Ball_ARC170_Location_Set")
 		RepublicHeroes:Add_Fighter_Set("Warthog_Republic_Z95_Location_Set")

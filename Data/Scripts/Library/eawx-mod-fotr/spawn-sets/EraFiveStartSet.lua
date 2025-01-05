64f6a495095e58cab7521997b38184510c05c01b
SFGeekFromK <swjacklee@gmail.com>
2025-01-05 11:34:58 +0900

    whitespace

--- Data/Scripts/Library/eawx-mod-fotr/spawn-sets/EraFiveStartSet.lua
+++ Data/Scripts/Library/eawx-mod-fotr/spawn-sets/EraFiveStartSet.lua
@@ -1,23 +1,23 @@
 return {
 	["EMPIRE"] = {
-	["CORUSCANT"] = {"Yoda_Eta_Team", "Pestage_Team", "Fordo2_Team"},
-	["CARIDA"] = { "Obi_Wan_Eta_Team", "Cody2_Team"},
-	["BOTHAWUI"] = {"Yularen_Integrity", "Rex2_Team", "Anakin_Eta_Team"},
-	["ONDERON"] = {"Romodi_Team", "Dodonna_Ardent"},
-	["ERIADU"] = {"Tarkin_Venator"},
-	["KASHYYYK"] = {"Delta_Squad"},
-	["KUAT"] = {"Rahm_Kota_Team", "Gentis_Team"},
-	["RENDILI"] = {"Plo_Koon_Delta_Team", "Dallin_Kebir"}
+		["CORUSCANT"] = {"Yoda_Eta_Team", "Pestage_Team", "Fordo2_Team"},
+		["CARIDA"] = { "Obi_Wan_Eta_Team", "Cody2_Team"},
+		["BOTHAWUI"] = {"Yularen_Integrity", "Rex2_Team", "Anakin_Eta_Team"},
+		["ONDERON"] = {"Romodi_Team", "Dodonna_Ardent"},
+		["ERIADU"] = {"Tarkin_Venator"},
+		["KASHYYYK"] = {"Delta_Squad"},
+		["KUAT"] = {"Rahm_Kota_Team", "Gentis_Team"},
+		["RENDILI"] = {"Plo_Koon_Delta_Team", "Dallin_Kebir"}
 	},
 	["REBEL"] = {
-	["RAXUS_SECOND"] = {"Lucid_Voice", "Dooku_Team", "Trench_Invulnerable"},
-	["BOZ_PITY"] = {"Durge_Team", "Ventress_Team", --[[FotR_Enhanced]]"TJ912_Recusant"},
-	["SERENNO"] = {"Grievous_Team_Recusant", "Colicoid_Swarm", "Sora_Bulq_Team"},
-	["FELUCIA"] = {"Kalani_Team"},
-	["HYPORI"] = {"TF1726_Munificent"},
-	["PAMMANT"] = {"Gunray_Team", "Harsol_Munificent", "AutO_Providence"},
-	["UTAPAU"] = {"Poggle_Team"},
-	},
+		["RAXUS_SECOND"] = {"Lucid_Voice", "Dooku_Team", "Trench_Invulnerable"},
+		["BOZ_PITY"] = {"Durge_Team", "Ventress_Team", --[[FotR_Enhanced]]"TJ912_Recusant"},
+		["SERENNO"] = {"Grievous_Team_Recusant", "Colicoid_Swarm", "Sora_Bulq_Team"},
+		["FELUCIA"] = {"Kalani_Team"},
+		["HYPORI"] = {"TF1726_Munificent"},
+		["PAMMANT"] = {"Gunray_Team", "Harsol_Munificent", "AutO_Providence"},
+		["UTAPAU"] = {"Poggle_Team"},
+		},
 	["HUTT_CARTELS"] = {
 		["GAMORR"]  = {
 			"Zorba_The_Hutt_Team",
@@ -42,6 +42,6 @@ return {
         }
 	},
 	["INDEPENDENT_FORCES"] = {
-	["FLORRUM"] = {"Hondo_Ohnaka_Team"}
+		["FLORRUM"] = {"Hondo_Ohnaka_Team"}
 	}
 }
